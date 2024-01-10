deb5f1a1b550253a3341a3d89b59203976da9e93
Indu <wjsdlsdn1158@naver.com>
2024-01-10 15:59:26 +0900

    json 썸네일 이미지 표기

--- data_pre.py
+++ data_pre.py
@@ -6,17 +6,14 @@ from haversine import haversine
 here = (37.29963, 127.0340)  #Latitude, Longitude
 
 # csv 파일 경로
-df = pd.read_csv('경기대양식.csv')
+df = pd.read_csv('./csv_data/경기대양식.csv')
 
-df_data= df[["이름", "도로명주소", "방문자 평점", "상세페이지URL", "위도", "경도"]]
-df_data["거리"] = ''
-for item in df_data:
-    df_data["거리"] = haversine(here, (df_data["위도"], df_data["경도"]), unit = 'm')
-
+df_data= df[["이름", "도로명주소", "방문자 평점", "상세페이지URL", "위도", "경도", "썸네일이미지URL"]]
 df_data = df_data.dropna()
 
-print(df_data.head())
-'''
+print(df_data)
+
 df_data.to_json('usa.json', force_ascii=False, orient= 'records', indent=4)
+'''
 df_data.to_csv("usa.csv", index=False, encoding="utf-8-sig")
 '''
